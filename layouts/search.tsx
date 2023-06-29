d18301055ced8710ff88e2ccb5a9f190b33b05f5
不狸 <junyu.junyujiang@alibaba-inc.com>
2023-06-29 22:24:29 +0800

    fix: currentTagName is always undefined

--- layouts/search.tsx
+++ layouts/search.tsx
@@ -27,10 +27,9 @@ export const SearchLayout: React.VFC<Props> = ({ tags, posts, currentTag }) => {
     return [];
   }, [posts, searchValue]);
 
-  const currentTagName: string | undefined = useMemo(() => {
-    if (!currentTag) return undefined;
-    return getTagDataBySlug(currentTag as TagSlug)?.name;
-  }, [currentTag]);
+  // const currentTagName: string | undefined = useMemo(() => {
+  //   return getTagDataBySlug(currentTag as TagSlug)?.name ?? currentTag;
+  // }, [currentTag]);
 
   if (!locale) return null;
 
@@ -39,7 +38,7 @@ export const SearchLayout: React.VFC<Props> = ({ tags, posts, currentTag }) => {
       <div className="relative my-2">
         <input
           type="text"
-          placeholder={currentTag ? `${locale.POST.SEARCHIN} #${currentTagName}` : locale.POST.SEARCH}
+          placeholder={currentTag ? `${locale.POST.SEARCHIN} #${currentTag}` : locale.POST.SEARCH}
           className="block py-2 px-4 w-full text-black dark:text-white bg-gray-50 dark:bg-gray-700 rounded-lg border-2 border-gray-300"
           onChange={(e) => setSearchValue(e.target.value)}
         />
